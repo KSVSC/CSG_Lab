80c447b5fb5972f8c3d98f6f02d7c5e0ff53e6c4
KSVSC <40667516+KSVSC@users.noreply.github.com>
2018-07-10 14:47:44 +0530

    c program created

---
+++ MatrixMult.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include <time.h>
+#include<string.h> 
+#include<stdlib.h>
+
+long a[5000][5000],b[5000][5000],p[5000][5000];
+int main(int num,char *args[])
+{
+	int n1=atoi(args[1]);
+	long n=n1;
+ 	clock_t start, end;
+ 	 	
+	for(long i=0;i<n;i++)
+	{
+		for(long j=0;j<n;j++)
+		{
+			a[i][j]=i+j;
+		}
+	}
+			
+	for(long i=0;i<n;i++)
+	{
+		for(long j=0;j<n;j++)
+		{
+			b[i][j]=i+(2*j);
+		}
+	}
+	start = clock();		
+
+	long c,d,k,sum=0;
+	for(c=0;c<n;c++){
+		for(d=0;d<n;d++)
+		{
+			for(k=0;k<n;k++)
+			{
+				sum=sum+a[c][k]*b[k][d];
+			}
+			p[c][d]=sum;
+			sum=0;
+		}
+	}
+	
+	 end = clock();
+	 
+	 long t=(end-start);
+	 long t1=t/CLOCKS_PER_SEC;
+	 
+	
+	return t;
+}
+	
